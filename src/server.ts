6b720428f455375eb74afa5cdeb5cab84b4ca714
vinnilmg <vini.delimagomes@gmail.com>
2021-02-22 21:15:03 -0300

    aula01 finalizacao

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,13 @@ import express from 'express';
 
 const app = express();
 
+//routes
+app.get('/', (request, response) => {
+    return response.json({message: 'Hello world NLW04'});
+})
+
+app.post('/', (request, response) => {
+    return response.json({message: 'Dados salvos.'});
+})
+
 app.listen(5050, () => console.log("Server is running!!")); //criar servidor
